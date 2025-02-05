4c21aae132691d32159f6e29598ef0c8604924ba
Sanjarbek <sanjarbek@example.com>
2025-02-05 10:34:25 +0500

    Fix for deployment

--- resources/views/dashboard/my_Quizzes.php
+++ resources/views/dashboard/my_Quizzes.php
@@ -105,7 +105,7 @@
                     </div>
                     <div class="flex justify-between">
                         <a href="/dashboard/quizzes/${quiz.id}/update" class="text-indigo-600 hover:text-indigo-800">Edit</a>
-                        <button class="text-green-600 hover:text-green-800">View Results</button>
+                        <button class="text-green-600 hover:text-green-800" onclick="copyContent('${quiz.unique_value}')" >Share</button>
                         <button class="text-red-600 hover:text-red-800" onclick="deleteQuiz(${quiz.id})">Delete</button>
                     </div>
                 </div>
@@ -131,6 +131,15 @@
                 .catch((error) => {
                     alert('Internetga qarang')
                 });
+        }
+    }
+
+    const copyContent = async (text) => {
+        try{
+            await navigator.clipboard.writeText(text);
+            alert('Content copied to clipboard')
+        }catch (err) {
+            console.error('Failed to copy: ',err);
         }
     }
 
