20fb10cda36bcc84690a558693709533e2df43e6
Sanjarbek <sanjarbek@example.com>
2025-02-05 07:04:29 +0500

    Fix for deployment

--- boostrap.php
+++ boostrap.php
@@ -1,7 +1,5 @@
 <?php
 
-ini_set('display_errors', 1);
-ini_set('error_reporting', E_ALL);
 
 
 require 'vendor/autoload.php';
@@ -10,5 +8,11 @@ date_default_timezone_set("Asia/Tashkent");
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
+
+if($_ENV['APP_DEBUG']) {
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors',1);
+}
 
 require 'helpers.php';
