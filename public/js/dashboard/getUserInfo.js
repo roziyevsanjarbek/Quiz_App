24963a691bf6384a352613d53d8907a7e414bf8c
Sanjarbek <sanjarbek@example.com>
2025-02-05 09:54:05 +0500

    Fix for deployment

--- public/js/dashboard/getUserInfo.js
+++ public/js/dashboard/getUserInfo.js
@@ -1,5 +1,5 @@
 async function user() {
-    const { default: apiFetch } = await import("../utils/apiFetch.js");
+    const { default: apiFetch } = await import("/public/ls/utils/apiFetch.js");
     await apiFetch( "/users/getInfo",  { method: "GET" } )
         .then((user) => {
             document.getElementById("userName").innerText = user.data.full_name;
